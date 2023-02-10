4bb952f50ee4f33426b90603d2bc1c49fa84b440
Andres Velasquez <andresvel00@gmail.com>
2023-02-10 18:03:41 -0500

    fix to query files

--- load_data.py
+++ load_data.py
@@ -16,10 +16,19 @@ def truncate_table(table: str) -> None:
     session.close()
     engine.dispose()
 
+def create_views():
+    engine = create_engine(URI, pool_size=10, max_overflow=20)
+    with engine.connect() as connection:
+        query = open("sql/trips_bronze.sql").read()
+        connection.execute(text(query))
+        query = open("sql/trips_silver.sql").read()
+        connection.execute(text(query))
+
 def load_data(df: ddf) -> None:
     table = "trips_raw"
-    truncate_table(table, URI)
+    truncate_table(table)
     df.to_sql(table, URI, if_exists="append", index=False, chunksize=100_000, method="multi", parallel=True)
+    create_views()
 
 def get_weekly_average_by_bounding_box(min_lat: float, min_lon: float, max_lat: float, max_lon: float):
     query = open("sql/trips_gold_weekly_average_by_bounding_box.sql").read().format(min_lon=min_lon, min_lat=min_lat, max_lon=max_lon, max_lat=max_lat)
